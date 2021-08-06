f6487984fd2e3f68c14aa4f57bdeb62c314d4d03
ys <s__yang@163.com>
2021-08-06 15:20:11 +0800

    123

---
+++ test01/src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;
+
+/**
+ * @Package: com.bjpowernode.domain
+ * @ClassName: Person
+ * @Author: YS
+ * @CreateTime: 2021/8/6 15:04
+ * @Description:
+ */
+public class Person {
+
+}
